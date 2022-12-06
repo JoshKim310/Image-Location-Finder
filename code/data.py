431e2f7045d9cffc894b6981fb59f3b975644228
Vincente <vba16@sfu.ca>
2022-12-06 11:08:48 -0800

    added code file

---
+++ code/data.py
@@ -0,0 +1,31 @@
+import sys
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+from pyspark.sql import SparkSession, functions, types, Row
+spark = SparkSession.builder.appName('The data').getOrCreate()
+assert spark.version >= '2.4' # make sure we have Spark 2.4+
+spark.sparkContext.setLogLevel('WARN')
+sc = spark.sparkContext
+spark.conf.set("spark.sql.session.timeZone", "UTC")
+
+
+
+amenity_schema = types.StructType([
+    types.StructField('lat', types.DoubleType(), nullable=False),
+    types.StructField('lon', types.DoubleType(), nullable=False),
+    types.StructField('timestamp', types.TimestampType(), nullable=False),
+    types.StructField('amenity', types.StringType(), nullable=False),
+    types.StructField('name', types.StringType(), nullable=True),
+    types.StructField('tags', types.MapType(types.StringType(), types.StringType()), nullable=False),
+])
+
+
+def main(input, output):
+    file = spark.read.json(input, schema=amenity_schema)
+    file.show()
+
+
+
+if __name__ == '__main__':
+    inputs = sys.argv[1]
+    output = sys.argv[2]
+    main(inputs, output)
