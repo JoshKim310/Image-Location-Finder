ad2d15d77e89c160d6363fd0c78ee86fd4d7ec12
Vincente <vba16@sfu.ca>
2022-12-06 21:49:25 -0800

    Minor changes to getting coordinates functionality

--- code/data.py
+++ code/data.py
@@ -1,4 +1,6 @@
 import sys
+
+from sqlalchemy import null
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 from pyspark.sql import SparkSession, functions, types, Row
 spark = SparkSession.builder.appName('The data').getOrCreate()
@@ -21,29 +23,42 @@ amenity_schema = types.StructType([
     types.StructField('tags', types.MapType(types.StringType(), types.StringType()), nullable=False),
 ])
 
+#Converts the coords into decimal coordinates
+def decimal_coords(coords, ref):
+    decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
+    if (ref == "S" or ref == "W"):
+        decimal_degrees = -decimal_degrees
+    return decimal_degrees
 
 
-def main(photoPath):
-
-    
-    #Uses the path of the image and extracts data from it
-    with open(photoPath, 'rb') as src:
+#Get the coordinates of the image by passing through the image path
+def image_coordinates(img_path):
+    coords = null
+    #Uses the path of the image and exif extracts data from it
+    with open(img_path, 'rb') as src:
         img = Image(src)
 
         #If image has exif data it prints it out, if not then that also gets printed out
         if img.has_exif:
-            info = f"has the EXIF {img.exif_version}"
+            #Makes call to the function that calculates the longitude and latitude of image
+            x = decimal_coords(img.gps_latitude, img.gps_latitude_ref)
+            y = decimal_coords(img.gps_longitude, img.gps_longitude_ref)
+            coords = (x, y)
+            
         else:
-            info = "does not contain any EXIF information"
-        print(f"Image {src.name}: {info}")
-        
+            print("The image has no EXIF information")
+            return
+        print(f"Was taken: {img.datetime_original}, and has coordinates:{coords}")
+        return coords
 
-    
 
-    image = PILimg.open(photoPath)
-    print(image)
+def main(photoPath):
     
-    #image_coordinates(photoPath)
+    imgCoords = image_coordinates(photoPath)
+    if (not imgCoords):
+        print("Error")
+        return
+
 
 
 
