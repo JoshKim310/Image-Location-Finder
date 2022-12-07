541955d4c52e435d90ac3aee5adcbaf28e1461a7
Vincente <vba16@sfu.ca>
2022-12-06 18:22:52 -0800

    Added code to main that reads the exif data of a image

--- code/data.py
+++ code/data.py
@@ -6,9 +6,12 @@ assert spark.version >= '2.4' # make sure we have Spark 2.4+
 spark.sparkContext.setLogLevel('WARN')
 sc = spark.sparkContext
 spark.conf.set("spark.sql.session.timeZone", "UTC")
+from PIL import Image as PILimg
+from PIL.ExifTags import TAGS
+from exif import Image
 
 
-
+#Schema for the data we were given about greater Vancouver (Prof Baker provided)
 amenity_schema = types.StructType([
     types.StructField('lat', types.DoubleType(), nullable=False),
     types.StructField('lon', types.DoubleType(), nullable=False),
@@ -19,13 +22,32 @@ amenity_schema = types.StructType([
 ])
 
 
-def main(input, output):
-    file = spark.read.json(input, schema=amenity_schema)
-    file.show()
+
+def main(photoPath):
+
+    
+    #Uses the path of the image and extracts data from it
+    with open(photoPath, 'rb') as src:
+        img = Image(src)
+
+        #If image has exif data it prints it out, if not then that also gets printed out
+        if img.has_exif:
+            info = f"has the EXIF {img.exif_version}"
+        else:
+            info = "does not contain any EXIF information"
+        print(f"Image {src.name}: {info}")
+        
+
+    
+
+    image = PILimg.open(photoPath)
+    print(image)
+    
+    #image_coordinates(photoPath)
+
 
 
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
-    output = sys.argv[2]
-    main(inputs, output)
+    main(inputs)
