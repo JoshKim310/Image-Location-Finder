035ecd5cb7c3d97737528d9ea784bad2be925216
Joshua <jka248@sfu.ca>
2022-12-09 21:41:40 -0800

    edit create-map

--- code/create-map.py
+++ code/create-map.py
@@ -8,7 +8,7 @@ def marker_map(map, df):
     for i, df in df.iterrows():
         coord = [df['latitude'], df['longitude']]
         folium.Marker(coord).add_to(map)
-    map.save('index.html')
+    map.save('marker_map.html')
 
 # generates a heat map with given data points
 def heat_map(map, df):
@@ -16,7 +16,7 @@ def heat_map(map, df):
     for i, df in df.iterrows():
         heatData.append([df['latitude'], df['longitude']])
     HeatMap(heatData).add_to(map)
-    map.save('index.html')
+    map.save('heat_map.html')
         
 
 def main(mapData):
@@ -24,8 +24,10 @@ def main(mapData):
     # read csv file that contains lat, lon, amenity type, and city data of a particular amenity
     df = pd.read_csv(mapData, names=['latitude', 'longitude', 'amenity', 'city'])
     map = folium.Map(location=[49.19, -122.7], tiles="OpenStreetMap", zoom_start=11)
-    
+    map1 = folium.Map(location=[49.19, -122.7], tiles="OpenStreetMap", zoom_start=11)
+
     marker_map(map, df)
+    heat_map(map1, df)
 
 
 if __name__ == '__main__':
